8d7786bd926f06a1ca4b6e40f861cdc23c476903
Mehak Sampla <mehak1219.becse24@chitkara.edu.in>
2025-11-09 09:29:09 +0530

    server javascript

---
+++ server.js
@@ -0,0 +1,12 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 6000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
